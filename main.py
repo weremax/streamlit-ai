9b3be7783c4dfa9c153903abf48b0c0c8b97f668
Christopher Blankenship <weremax@Christophers-MacBook-Pro.local>
2026-02-12 21:47:29 -0500

    fixing formatting on main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ st.title("Quick Chat")
 model = OllamaLLM(model="qwen2.5-coder:1.5b")
 
 def generate_response(input_text):
-  st.info(model.invoke(input_text))
+    st.info(model.invoke(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'Replace this text with your question or statement.')
-  submitted = st.form_submit_button('Submit')
-  if submitted:generate_response(text)
+    text = st.text_area('Enter text:', 'Replace this text with your question or statement.')
+    submitted = st.form_submit_button('Submit')
+    if submitted:generate_response(text)
 
